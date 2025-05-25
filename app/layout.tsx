cdfa8c00452ab2eac4012673ad61227b206c0b48
netfishx <netfishx@gmail.com>
2025-05-25 20:43:08 +0900

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
+import { ThemeProvider } from "@/components/ui/theme-provider";
 import "./globals.css";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: 'next demo',
+}
 
 export default function RootLayout({
   children,
@@ -7,9 +12,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <NuqsAdapter>{children}</NuqsAdapter>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NuqsAdapter>{children}</NuqsAdapter>
+        </ThemeProvider>
       </body>
     </html>
   );
